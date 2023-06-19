1b1d40d5ffccdd16d086dc62ba3bc635e6357a10
Adin Ermie <Adin.Ermie@outlook.com>
2023-06-19 16:06:54 -0400

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "tfc-tutorials"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
